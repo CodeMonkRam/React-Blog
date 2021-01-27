de22ca1caceccea6a8e9c1e8b61c018f6a6fffae
CodeMonkRam <ramamoorthimuthuraj96@gmail.com>
2021-01-27 22:36:26 +0530

    connecting dB

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticleList from '../Components/ArticleList';
 import articleContent from "./Article-content"
 import { NotFoundPage } from './NotFoundPage';
@@ -9,12 +9,27 @@ const ArticlePage = ({ match }) => {
   const name= match.params.name;
   const article =articleContent.find(article => article.name === name);  
   
+  const [articleInfo, setArticleInfo] = useState({ upvote: 0, addcomments: [] });
+
+  useEffect(() => {
+      const fetchData = async () => {
+            const result = await fetch(`http://localhost:8000/api/articles/${name}`)
+            const body = await result.json();
+            console.log(body)
+            setArticleInfo(body)
+
+      }
+      fetchData();
+      setArticleInfo({ upvote: Math.ceil(Math.random()*10) });
+  },[name]);
+
   if(!article) return <NotFoundPage/>
   
   const otherArticle = articleContent.filter(article => article.name !== name);
   return(
     <>
-    <h1> {article.title} pages</h1>   
+    <h1> {article.title} pages</h1>  
+    <p>This post has been upvoted {articleInfo.upvote} times</p>
     {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
     ))} 
@@ -23,7 +38,7 @@ const ArticlePage = ({ match }) => {
 
     </>
 
-);
+); 
     }
 export default ArticlePage;
 
