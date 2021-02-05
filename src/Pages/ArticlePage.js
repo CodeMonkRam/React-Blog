d6b1d44d7f7296b8c42cf7806bc98f5fa108db8c
CodeMonkRam <ramamoorthimuthuraj96@gmail.com>
2021-02-06 00:20:04 +0530

    Your awesome message

--- src/Pages/ArticlePage.js
+++ src/Pages/ArticlePage.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ArticleList from '../Components/ArticleList';
 import articleContent from "./Article-content"
 import { NotFoundPage } from './NotFoundPage';
- 
+import CommentsList from '../Components/CommentsList';
+import UpvotesSection from '../Components/UpvotesSection';
+import AddCommentForm from '../Components/AddCommentForm';
 
 
 const ArticlePage = ({ match }) => {
@@ -12,15 +14,15 @@ const ArticlePage = ({ match }) => {
   const [articleInfo, setArticleInfo] = useState({ upvote: 0, addcomments: [] });
 
   useEffect(() => {
-      const fetchData = async () => {
-            const result = await fetch(`http://localhost:8000/api/articles/${name}`)
-            const body = await result.json();
-            console.log(body)
-            setArticleInfo(body)
-
-      }
-      fetchData();
-      setArticleInfo({ upvote: Math.ceil(Math.random()*10) });
+    const fetchData = async () => {
+        const result = await fetch(`/api/articles/${name}`);
+        const body = await result.json();
+       // console.log(body);
+        setArticleInfo(body);
+        
+    }
+    fetchData();
+     //setArticleInfo({ upvote: Math.ceil(Math.random()*10) });
   },[name]);
 
   if(!article) return <NotFoundPage/>
@@ -29,10 +31,13 @@ const ArticlePage = ({ match }) => {
   return(
     <>
     <h1> {article.title} pages</h1>  
-    <p>This post has been upvoted {articleInfo.upvote} times</p>
+    <UpvotesSection articleName={name} upvotes= {articleInfo.upvote} setArticleInfo={setArticleInfo}/>
     {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
     ))} 
+
+ <CommentsList comments={articleInfo.addcomments}/>  
+ <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/> 
 <h3>Other Articles:</h3>
 <ArticleList article={otherArticle} />
 
